14ebdbb2d235a88585da95a292473a83b7806e11
tfrank <tfrank@vmware.com>
2023-09-08 13:26:05 -0400

    Remove obj folders and add dependencies.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Steeltoe.Connector.PostgreSql;
+using Steeltoe.Extensions.Configuration.Kubernetes.ServiceBinding;
+using Steeltoe.Management.Endpoint;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddKubernetesServiceBindings();
+builder.Services.AddPostgresHealthContributor(builder.Configuration);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.AddAllActuators();
 
 var app = builder.Build();
 
@@ -20,6 +30,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
